56cee52aa092bf36287b0154109b3f05b8ca0fe8
Thibault Pensec <39826516+thibauult@users.noreply.github.com>
2023-11-02 18:07:40 +0100

    Now using Next.js (#2)

--- src/components/Layout.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Layout from './Layout';
-
-test('renders children in layout', () => {
-    render(<Layout title={"Hello"}>Hello World</Layout>);
-    const title = screen.getByText('Hello World')
-    expect(title).toBeInTheDocument();
-});
--- src/components/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import './Loader.css'
-
-function Loader() {
-    return (
-        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    )
-}
-
-export default Loader
