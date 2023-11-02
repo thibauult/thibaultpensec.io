e5a9f57c843f4e403a6dc5e64f9c18f2e7bed6e4
tibus29 <thibault.pensec@gmail.com>
2023-11-02 17:24:52 +0100

    Now using Next.js
    Signed-off-by: tibus29 <thibault.pensec@gmail.com>

--- src/components/Loader.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Loader from './Loader';
-
-test('renders children in layout', () => {
-    const { container } = render(<Loader />);
-    expect(container.firstChild).toHaveClass('lds-roller')
-});
--- src/pages/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-function NotFound() {
-  return (
-      <h1>Page not found</h1>
-  )
-}
-
-export default NotFound
